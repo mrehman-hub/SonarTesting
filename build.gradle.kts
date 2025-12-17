6b8d21cdc0441bde5bcc7eba0b43d3ef8b4f1e10
rehman <mrehman@brightbyteconsulting.dev>
2025-12-17 17:42:44 +0500

    added yml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,17 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("org.sonarqube") version "2.5"
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectName", "SonarTesting")
+        property("sonar.projectKey", "mrehman-hub_SonarTesting")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.login", System.getenv("SONAR_TOKEN"))
+        property("sonar.sources","src/main/java")
+        property("sonar.language","java")
+        property("sonar.sourceEncoding", "UTF-8")
+    }
 }
